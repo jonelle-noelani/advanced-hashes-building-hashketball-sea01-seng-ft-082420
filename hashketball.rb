cd28a6f8c1118caefe99845aabfb0be85b8df6fd
Jonelle Noelani Yacapin <jonelle_noelani@hotmail.com>
2020-08-16 09:47:53 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,9 +2,9 @@ def game_hash
 game_hash = {
 :home => { 
 	:team_name => "Brooklyn Nets",
-	:colors => [“Black”, “White”],
+	:colors => ["Black", "White"],
 	:players => [ 
-		{:player_name => “Alan Anderson”,
+		{:player_name => "Alan Anderson",
 		:number => 0,
 		:shoe => 16,
 		:points => 22,

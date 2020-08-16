642d3590c1e58125e8e086b2f780e7818cc7d221
Jonelle Noelani Yacapin <jonelle_noelani@hotmail.com>
2020-08-16 09:40:33 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -99,7 +99,8 @@ game_hash = {
 		:blocks => 1,
 		:slam_dunks => 0},
 
-		{:player_name => “Kemba Walker”,
+		{
+		:player_name => “Kemba Walker”,
 		:number => 33,
 		:shoe => 15,
 		:points => 6,
